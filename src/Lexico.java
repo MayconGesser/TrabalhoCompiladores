a97e3d06298a2f7c7879a02b748bb970adea3f49
Maycon <maycon.antonio.gesser@gmail.com>
2018-11-12 01:02:19 -0200

    adiciona métodos para parsing léxico

--- src/Lexico.java
+++ src/Lexico.java
@@ -1,7 +1,48 @@
+import java.util.HashMap;
+import java.util.Set;
+
 public class Lexico implements Constants
 {
+	private String sourceCode; 
+	private int currentPosition; 
+	private char currentChar;
+	private final char space = ' ';
+	private final HashMap<String,Integer> lexemesToIds = new HashMap();
+	//inicializador de instância
+	{
+		int offsetToId = 2; 
+		for(int i = 0; i<lexemes.length; i++) {
+			lexemesToIds.put(lexemes[i], i+offsetToId);
+		}		
+	}
+	private final Set<String> possibleLexemes = lexemesToIds.keySet();
+	
+	public Lexico(String sourceCode) {
+		this.sourceCode = sourceCode;
+		currentPosition = 0;
+	}
+	
     public Token nextToken() throws LexicalError
     {
-        return null;
+    	String potentialLexeme;
+    	Token t = null;		//q lixo de código q esse lixo de linguagem obriga a escrever...
+    	char[] sequenceBuffer = new char[10];
+    	currentChar = sourceCode.charAt(currentPosition);
+    	//todo Token, por mais valido q seja,
+    	//eh separado por espaço
+    	while(currentChar != space) {    		
+    		sequenceBuffer[currentPosition] = currentChar;
+    		currentPosition++;
+    		currentChar = sourceCode.charAt(currentPosition);
+    	}
+    	potentialLexeme = new String(sequenceBuffer);
+    	if(possibleLexemes.contains(potentialLexeme)) {
+    		int tokenId = lexemesToIds.get(potentialLexeme);
+    		t = new Token(tokenId,potentialLexeme,currentPosition);
+    	}
+    	else {
+    		throw new LexicalError("Erro léxico na posição " + currentPosition, currentPosition);
+    	}
+    	return t;
     }
 }
