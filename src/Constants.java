4849b00f46546701d01c21fbb7e9942828f43d42
Maycon <maycon.antonio.gesser@gmail.com>
2018-11-14 13:40:06 -0200

    adiciona arquivos gerados pelo GALS

--- src/Constants.java
+++ src/Constants.java
@@ -3,53 +3,61 @@ public interface Constants extends ScannerConstants, ParserConstants
     int EPSILON  = 0;
     int DOLLAR   = 1;
 
-    int t_TOKEN_2 = 2; //":"
-    int t_TOKEN_3 = 3; //"<"
-    int t_TOKEN_4 = 4; //"+"
-    int t_TOKEN_5 = 5; //"/"
-    int t_TOKEN_6 = 6; //"]"
-    int t_TOKEN_7 = 7; //"="
-    int t_TOKEN_8 = 8; //")"
-    int t_TOKEN_9 = 9; //"*"
-    int t_TOKEN_10 = 10; //">"
-    int t_TOKEN_11 = 11; //","
-    int t_TOKEN_12 = 12; //"}"
-    int t_TOKEN_13 = 13; //"["
-    int t_TOKEN_14 = 14; //"{"
-    int t_TOKEN_15 = 15; //";"
-    int t_TOKEN_16 = 16; //"."
-    int t_TOKEN_17 = 17; //"("
-    int t_TOKEN_18 = 18; //"-"
-    int t_TOKEN_19 = 19; //"<="
-    int t_TOKEN_20 = 20; //">="
-    int t_TOKEN_21 = 21; //":="
-    int t_TOKEN_22 = 22; //"<>"
-    int t_id = 23;
-    int t_num_int = 24;
-    int t_num_real = 25;
-    int t_literal = 26;
-    int t_ou = 27;
-    int t_e = 28;
-    int t_nao = 29;
-    int t_falso = 30;
-    int t_verdadeiro = 31;
-    int t_programa = 32;
-    int t_inteiro = 33;
-    int t_real = 34;
-    int t_booleano = 35;
-    int t_caracter = 36;
-    int t_cadeia = 37;
-    int t_metodo = 38;
-    int t_ref = 39;
-    int t_val = 40;
-    int t_se = 41;
-    int t_entao = 42;
-    int t_senao = 43;
-    int t_leia = 44;
-    int t_escreva = 45;
-    int t_retorne = 46;
-    int t_div = 47;
-    int t_enquanto = 48;
-    int t_faca = 49;
+    int t_pr = 2;
+    int t_id = 3;
+    int t_num_int = 4;
+    int t_num_real = 5;
+    int t_literal = 6;
+    int t_booleano = 7;
+    int t_cadeia = 8;
+    int t_caracter = 9;
+    int t_de = 10;
+    int t_div = 11;
+    int t_e = 12;
+    int t_enquanto = 13;
+    int t_entao = 14;
+    int t_escreva = 15;
+    int t_faca = 16;
+    int t_falso = 17;
+    int t_fim = 18;
+    int t_funcao = 19;
+    int t_inicio = 20;
+    int t_inteiro = 21;
+    int t_leia = 22;
+    int t_metodo = 23;
+    int t_nao = 24;
+    int t_ou = 25;
+    int t_procedimento = 26;
+    int t_programa = 27;
+    int t_real = 28;
+    int t_ref = 29;
+    int t_retorne = 30;
+    int t_se = 31;
+    int t_senao = 32;
+    int t_val = 33;
+    int t_var = 34;
+    int t_verdadeiro = 35;
+    int t_vetor = 36;
+    int t_TOKEN_37 = 37; //":"
+    int t_TOKEN_38 = 38; //"<"
+    int t_TOKEN_39 = 39; //"+"
+    int t_TOKEN_40 = 40; //"/"
+    int t_TOKEN_41 = 41; //"]"
+    int t_TOKEN_42 = 42; //"="
+    int t_TOKEN_43 = 43; //")"
+    int t_TOKEN_44 = 44; //"*"
+    int t_TOKEN_45 = 45; //">"
+    int t_TOKEN_46 = 46; //","
+    int t_TOKEN_47 = 47; //"}"
+    int t_TOKEN_48 = 48; //"["
+    int t_TOKEN_49 = 49; //"{"
+    int t_TOKEN_50 = 50; //";"
+    int t_TOKEN_51 = 51; //"."
+    int t_TOKEN_52 = 52; //"("
+    int t_TOKEN_53 = 53; //"-"
+    int t_TOKEN_54 = 54; //"<="
+    int t_TOKEN_55 = 55; //">="
+    int t_TOKEN_56 = 56; //":="
+    int t_TOKEN_57 = 57; //"<>"
 
 }
