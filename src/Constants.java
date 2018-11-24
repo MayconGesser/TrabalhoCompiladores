02224ccec2d430fae94baf4d709036923cf7e1bc
hppyft <leo@wiitec.com.br>
2018-11-24 17:30:15 -0200

    codigo do gals arrumado

--- src/Constants.java
+++ src/Constants.java
@@ -3,9 +3,9 @@ public interface Constants extends ScannerConstants, ParserConstants
     int EPSILON  = 0;
     int DOLLAR   = 1;
 
-    int t_id = 2;
-    int t_num_int = 3;
-    int t_num_real = 4;
+    int t_num_int = 2;
+    int t_num_real = 3;
+    int t_id = 4;
     int t_literal = 5;
     int t_booleano = 6;
     int t_cadeia = 7;
@@ -18,45 +18,43 @@ public interface Constants extends ScannerConstants, ParserConstants
     int t_escreva = 14;
     int t_faca = 15;
     int t_falso = 16;
-    int t_fim = 17;
-    int t_funcao = 18;
-    int t_inicio = 19;
-    int t_inteiro = 20;
-    int t_leia = 21;
-    int t_metodo = 22;
-    int t_nao = 23;
-    int t_ou = 24;
-    int t_procedimento = 25;
-    int t_programa = 26;
-    int t_real = 27;
-    int t_ref = 28;
-    int t_retorne = 29;
-    int t_se = 30;
-    int t_senao = 31;
-    int t_val = 32;
-    int t_var = 33;
-    int t_verdadeiro = 34;
-    int t_vetor = 35;
-    int t_TOKEN_36 = 36; //":"
-    int t_TOKEN_37 = 37; //"<"
-    int t_TOKEN_38 = 38; //"+"
-    int t_TOKEN_39 = 39; //"/"
-    int t_TOKEN_40 = 40; //"]"
-    int t_TOKEN_41 = 41; //"="
-    int t_TOKEN_42 = 42; //")"
-    int t_TOKEN_43 = 43; //"*"
-    int t_TOKEN_44 = 44; //">"
-    int t_TOKEN_45 = 45; //","
-    int t_TOKEN_46 = 46; //"}"
-    int t_TOKEN_47 = 47; //"["
-    int t_TOKEN_48 = 48; //"{"
-    int t_TOKEN_49 = 49; //";"
-    int t_TOKEN_50 = 50; //"."
-    int t_TOKEN_51 = 51; //"("
-    int t_TOKEN_52 = 52; //"-"
-    int t_TOKEN_53 = 53; //"<="
-    int t_TOKEN_54 = 54; //">="
-    int t_TOKEN_55 = 55; //":="
-    int t_TOKEN_56 = 56; //"<>"
+    int t_funcao = 17;
+    int t_inteiro = 18;
+    int t_leia = 19;
+    int t_metodo = 20;
+    int t_nao = 21;
+    int t_ou = 22;
+    int t_procedimento = 23;
+    int t_programa = 24;
+    int t_real = 25;
+    int t_ref = 26;
+    int t_retorne = 27;
+    int t_se = 28;
+    int t_senao = 29;
+    int t_val = 30;
+    int t_var = 31;
+    int t_verdadeiro = 32;
+    int t_vetor = 33;
+    int t_TOKEN_34 = 34; //":"
+    int t_TOKEN_35 = 35; //"<"
+    int t_TOKEN_36 = 36; //"+"
+    int t_TOKEN_37 = 37; //"/"
+    int t_TOKEN_38 = 38; //"]"
+    int t_TOKEN_39 = 39; //"="
+    int t_TOKEN_40 = 40; //")"
+    int t_TOKEN_41 = 41; //"*"
+    int t_TOKEN_42 = 42; //">"
+    int t_TOKEN_43 = 43; //","
+    int t_TOKEN_44 = 44; //"}"
+    int t_TOKEN_45 = 45; //"["
+    int t_TOKEN_46 = 46; //"{"
+    int t_TOKEN_47 = 47; //";"
+    int t_TOKEN_48 = 48; //"."
+    int t_TOKEN_49 = 49; //"("
+    int t_TOKEN_50 = 50; //"-"
+    int t_TOKEN_51 = 51; //"<="
+    int t_TOKEN_52 = 52; //">="
+    int t_TOKEN_53 = 53; //":="
+    int t_TOKEN_54 = 54; //"<>"
 
 }
--- src/Semantico.java
+++ src/Semantico.java
@@ -2,6 +2,6 @@ public class Semantico implements Constants
 {
     public void executeAction(int action, Token token)	throws SemanticError
     {
-        System.out.println("AÃ§Ã£o #"+action+", Token: "+token);
+        System.out.println("Ação #"+action+", Token: "+token);
     }	
 }
